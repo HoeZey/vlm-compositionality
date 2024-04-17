2f03a7588e5ce760def66c0207b8c44866a6892b
Matteo Nulli <matteo.nulli@gmail.com>
2024-04-17 14:47:05 +0200

    created a class for winoground evaluation

--- benchmarks/winoground/winoground_evaluation.py
+++ benchmarks/winoground/winoground_evaluation.py
@@ -5,84 +5,97 @@ import numpy as np
 from datasets import load_dataset
 
 
-
-def text_correct(result):
-    return result["c0_i0"] > result["c1_i0"] and result["c1_i1"] > result["c0_i1"]
-
-def image_correct(result):
-    return result["c0_i0"] > result["c0_i1"] and result["c1_i1"] > result["c1_i0"]
-
-def group_correct(result):
-    return image_correct(result) and text_correct(result)
-
-
-def evaluate_open_clip_on_winoground(model_name, pretrained):
-    """ 
-    return:
-    {
-        "Winoground_accuracies": {
-            "text_score": x,
-            "image_score": y,
-            "group_score": z
-        }
-    }
-    where 
-    - text_score is the accuracy of the model on matching text with the correct image 
-    - image_score is the accuracy of the model on matching image with the correct text
-    - group_score is the accuracy of the model if both previous pairings are correct
+class Winoground_evaluation:
     """
-    # auth_token = "" 
-    auth_token = ""  # Replace with an auth token, which you can get from your huggingface account: Profile -> Settings -> Access Tokens -> New Token
-    winoground = load_dataset("facebook/winoground", use_auth_token=auth_token, trust_remote_code=True)["test"]
+    This class is used to evaluate the OpenCLIP model on the Winoground dataset
+    """
+    def __init__(self, model_name, pretrained):
+        self.model_name = model_name
+        self.pretrained = pretrained
 
-    model, _, preprocess  = open_clip.create_model_and_transforms(model_name, pretrained)
-    tokenizer = open_clip.get_tokenizer(model_name)
-
-    from tqdm import tqdm
-    winoground_clip_scores = []
-    for example in tqdm(winoground):
-        text = [example["caption_0"], example["caption_1"]]
-        text = tokenizer(text)
-
-        image0 = preprocess(example["image_0"].convert("RGB")).unsqueeze(0)
-        image1 = preprocess(example["image_1"].convert("RGB")).unsqueeze(0)
         
-        with torch.no_grad(), torch.cuda.amp.autocast():
-            image_features = model.encode_image(image0)
-            text_features = model.encode_text(text)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            text_features /= text_features.norm(dim=-1, keepdim=True)
+    def text_correct(self, result):
+        return result["c0_i0"] > result["c1_i0"] and result["c1_i1"] > result["c0_i1"]
 
-            output_i0 = (100.0 * image_features @ text_features.T)
-            c0_i0 = output_i0[0][0].item()
-            c1_i0 = output_i0[0][1].item()
+    def image_correct(self, result):
+        return result["c0_i0"] > result["c0_i1"] and result["c1_i1"] > result["c1_i0"]
 
-            image_features = model.encode_image(image1)
-            text_features = model.encode_text(text)
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-            text_features /= text_features.norm(dim=-1, keepdim=True)
-
-            # output_i1 = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-            output_i1 = (100.0 * image_features @ text_features.T)
-            c0_i1 = output_i1[0][0].item()
-            c1_i1 = output_i1[0][1].item()
+    def group_correct(self, result):
+        return Winoground_evaluation.image_correct(result) and Winoground_evaluation.text_correct(result)
 
 
-        winoground_clip_scores.append({"id" : example["id"], "c0_i0": c0_i0, "c0_i1": c0_i1, "c1_i0": c1_i0, "c1_i1": c1_i1})
+    def evaluate_open_clip_on_winoground(self, model_name, pretrained):
+        """ 
+        input: model_name (str) - the name of the model to be used (can be choosen out of open_clip.list_pretrained())
+                pretrained (str) - the name of the pre-trined weigths corresponding to the model_name 
+        return:
+        {
+            "Winoground_accuracies": {
+                "text_score": x,
+                "image_score": y,
+                "group_score": z
+            }
+        }
+        where 
+        - text_score is the accuracy of the model on matching text with the correct image 
+        - image_score is the accuracy of the model on matching image with the correct text
+        - group_score is the accuracy of the model if both previous pairings are correct
+        """
+        auth_token = "" 
+        # auth_token = "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"  # Replace with an auth token, which you can get from your huggingface account: Profile -> Settings -> Access Tokens -> New Token
+        winoground = load_dataset("facebook/winoground", use_auth_token=auth_token, trust_remote_code=True)["test"]
 
-    # evaluating accuracy
-   
-    text_correct_count = 0
-    image_correct_count = 0
-    group_correct_count = 0
-    for result in winoground_clip_scores:
-        text_correct_count += 1 if text_correct(result) else 0
-        image_correct_count += 1 if image_correct(result) else 0
-        group_correct_count += 1 if group_correct(result) else 0
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        denominator = len(winoground_clip_scores)
+        model, _, preprocess  = open_clip.create_model_and_transforms(model_name, pretrained, device=device)
+        tokenizer = open_clip.get_tokenizer(model_name)
+
+        from tqdm import tqdm
+        winoground_clip_scores = []
+        for example in tqdm(winoground):
+            text = [example["caption_0"], example["caption_1"]]
+            text = tokenizer(text)
+
+            image0 = preprocess(example["image_0"].convert("RGB")).unsqueeze(0).to(device)
+            image1 = preprocess(example["image_1"].convert("RGB")).unsqueeze(0).to(device)
+            
+            with torch.no_grad(), torch.cuda.amp.autocast():
+                
+                image_features = model.encode_image(image0)
+                text_features = model.encode_text(text)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                text_features /= text_features.norm(dim=-1, keepdim=True)
+
+                output_i0 = (100.0 * image_features @ text_features.T)
+                c0_i0 = output_i0[0][0].item()
+                c1_i0 = output_i0[0][1].item()
+
+                image_features = model.encode_image(image1)
+                text_features = model.encode_text(text)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                text_features /= text_features.norm(dim=-1, keepdim=True)
+
+                # output_i1 = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+                output_i1 = (100.0 * image_features @ text_features.T)
+                c0_i1 = output_i1[0][0].item()
+                c1_i1 = output_i1[0][1].item()
+
+
+            winoground_clip_scores.append({"id" : example["id"], "c0_i0": c0_i0, "c0_i1": c0_i1, "c1_i0": c1_i0, "c1_i1": c1_i1})
+
+        # evaluating accuracy
     
-    acc_val = {"text_score": text_correct_count/denominator, "image_score": image_correct_count/denominator, "group_score": group_correct_count/denominator}
+        text_correct_count = 0
+        image_correct_count = 0
+        group_correct_count = 0
+        for result in winoground_clip_scores:
+            text_correct_count += 1 if text_correct(result) else 0
+            image_correct_count += 1 if image_correct(result) else 0
+            group_correct_count += 1 if group_correct(result) else 0
 
-    return {"Winoground_accuracies": acc_val}
+            denominator = len(winoground_clip_scores)
+        
+        acc_val = {"text_score": text_correct_count/denominator, "image_score": image_correct_count/denominator, "group_score": group_correct_count/denominator}
+
+        return {"Winoground_accuracies": acc_val}
 
