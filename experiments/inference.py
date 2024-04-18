4fd8c1e2d46d24d6a73da40caa391ff91acab1c3
HoeZey <hoshelee@ymail.com>
2024-04-18 14:51:33 +0200

    Added cwd to path for inference to fix module not found error

--- experiments/inference.py
+++ experiments/inference.py
@@ -1,17 +1,19 @@
+import sys
 import os
-os.chdir('../')
+sys.path.append(os.getcwd())
 from models.model_wrappers import OpenClipWrapper
 from benchmarks.benchmark_wrappers import instantiate_benchmarks
 from collections import defaultdict
 
-models = [OpenClipWrapper()]
-benchmarks = instantiate_benchmarks()
 
-results = defaultdict(dict)
-for model in models:
-    for benchmark in benchmarks:
-        result = benchmark.evaluate(model)
-        print(f'{model.name:<5} {benchmark.name:<13} {result}')
-        results[model.name][benchmark.name] = result
+if __name__ == '__main__':
+    models = [OpenClipWrapper()]
+    benchmarks = instantiate_benchmarks()
 
-print(results)
+    results = defaultdict(dict)
+    for model in models:
+        for benchmark in benchmarks:
+            result = benchmark.evaluate(model)
+            print(f'{model.name:<5} {benchmark.name:<13} {result}')
+            results[model.name][benchmark.name] = result
+    print(results)
