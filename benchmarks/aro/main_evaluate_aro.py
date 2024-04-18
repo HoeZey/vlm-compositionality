5383a90e3b6c94145789608b7d0827136fb0e79b
Avik Pal <avikpal62@gmail.com>
2024-04-18 11:47:17 +0200

    Crerated main benchmark caller and added class module in ARO

--- benchmarks/aro/main_evaluate_aro.py
+++ benchmarks/aro/main_evaluate_aro.py
@@ -4,103 +4,98 @@ sys.path.append("..")
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
-
-#-----------------------------------------------------------------------------------
-import argparse
-import os
 import pandas as pd
 import torch
 
 from torch.utils.data import DataLoader
 import open_clip
 
-# from model_zoo import get_model
-# from dataset_zoo import get_dataset
-from benchmarks.aro.misc import seed_all, _default_collate, save_scores
+from benchmarks.aro.misc import seed_all, _default_collate
 from benchmarks.aro.model_zoo.clip_models import CLIPWrapper
-from benchmarks.aro.dataset_zoo.aro_datasets import VG_Relation, VG_Attribution, COCO_Order, Flickr30k_Order
-# from benchmark.aro.dataset_zoo.retrieval import COCO_Retrieval, Flickr30k_Retrieval
 
     
-def load_model(model_name, pretrained, device):
-    model, _, image_preprocess = open_clip.create_model_and_transforms(model_name, pretrained, device=device)
-    model = model.eval()
-    clip_model = CLIPWrapper(model, device) 
-    return clip_model, image_preprocess
-
-def load_dataset(dataset_name, image_preprocess=None, text_perturb_fn=None, image_perturb_fn=None, download=False, *args, **kwargs):
+class ARO_evaluation:
     """
-    Helper function that returns a dataset object with an evaluation function. 
-    dataset_name: Name of the dataset.
-    image_preprocess: Preprocessing function for images.
-    text_perturb_fn: A function that takes in a string and returns a string. This is for perturbation experiments.
-    image_perturb_fn: A function that takes in a PIL image and returns a PIL image. This is for perturbation experiments.
-    download: Whether to allow downloading images if they are not found.
+    This class is used to evaluate the OpenCLIP model on the ARO datasets
     """
-    if dataset_name == "VG_Relation": 
-        from benchmarks.aro.dataset_zoo.aro_datasets import get_visual_genome_relation
-        return get_visual_genome_relation(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    elif dataset_name == "VG_Attribution":
-        from benchmarks.aro.dataset_zoo.aro_datasets import get_visual_genome_attribution
-        return get_visual_genome_attribution(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    elif dataset_name == "COCO_Order":
-        from benchmarks.aro.dataset_zoo.aro_datasets import get_coco_order
-        return get_coco_order(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    elif dataset_name == "Flickr30k_Order":
-        from benchmarks.aro.dataset_zoo.aro_datasets import get_flickr30k_order
-        return get_flickr30k_order(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    # elif dataset_name == "COCO_Retrieval":
-    #     from benchmark.aro.dataset_zoo.retrieval import get_coco_retrieval
-    #     return get_coco_retrieval(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    # elif dataset_name == "Flickr30k_Retrieval":
-    #     from benchmark.aro.dataset_zoo.retrieval import get_flickr30k_retrieval
-    #     return get_flickr30k_retrieval(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
-    else:
-        raise ValueError(f"Unknown dataset {dataset_name}")
+    def __init__(self, model_name, pretrained) -> None:
+        self.model_name = model_name
+        self.pretrained = pretrained
 
 
-def evaluate_open_clip_aro(model_name, pretrained):
+    def load_model(self, model_name, pretrained, device):
+        model, _, image_preprocess = open_clip.create_model_and_transforms(model_name, pretrained, device=device)
+        model = model.eval()
+        clip_model = CLIPWrapper(model, device) 
+        return clip_model, image_preprocess
 
-    seed = 1
-    # model_name = "openai-clip:ViT-B/32"
-    # # device = "cpu"
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    if torch.backends.mps.is_available():
-        device = "mps"
-    elif torch.cuda.is_available():
-        device = "cuda"
-    else:
-        device = "cpu"
 
-    seed_all(seed)
-    # dataset_name = "VG_Relation" 
-    dataset_names =["VG_Relation", "VG_Attribution", "COCO_Order", "Flickr30k_Order"]
-    download = True
-    batch_size = 32
-    num_workers = 4
+    def load_dataset(self, dataset_name, image_preprocess=None, text_perturb_fn=None, image_perturb_fn=None, download=False, *args, **kwargs):
+        """
+        Helper function that returns a dataset object with an evaluation function. 
+        dataset_name: Name of the dataset.
+        image_preprocess: Preprocessing function for images.
+        text_perturb_fn: A function that takes in a string and returns a string. This is for perturbation experiments.
+        image_perturb_fn: A function that takes in a PIL image and returns a PIL image. This is for perturbation experiments.
+        download: Whether to allow downloading images if they are not found.
+        """
+        if dataset_name == "VG_Relation": 
+            from benchmarks.aro.dataset_zoo.aro_datasets import get_visual_genome_relation
+            return get_visual_genome_relation(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, 
+                                            image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
+        elif dataset_name == "VG_Attribution":
+            from benchmarks.aro.dataset_zoo.aro_datasets import get_visual_genome_attribution
+            return get_visual_genome_attribution(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, 
+                                                image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
+        elif dataset_name == "COCO_Order":
+            from benchmarks.aro.dataset_zoo.aro_datasets import get_coco_order
+            return get_coco_order(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, 
+                                image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
+        elif dataset_name == "Flickr30k_Order":
+            from benchmarks.aro.dataset_zoo.aro_datasets import get_flickr30k_order
+            return get_flickr30k_order(image_preprocess=image_preprocess, text_perturb_fn=text_perturb_fn, 
+                                    image_perturb_fn=image_perturb_fn, download=download, *args, **kwargs)
+        else:
+            raise ValueError(f"Unknown dataset {dataset_name}")
+
+
+    def evaluate_open_clip_on_aro(self):
+        seed = 1
+        if torch.backends.mps.is_available():
+            device = "mps"
+        elif torch.cuda.is_available():
+            device = "cuda"
+        else:
+            device = "cpu"
+
+        seed_all(seed)
+        dataset_names =["VG_Relation", "VG_Attribution", "COCO_Order", "Flickr30k_Order"]
+        download = True
+        batch_size = 32
+        num_workers = 4
+        
+        model, image_preprocess = self.load_model(self.model_name, self.pretrained, device)
+        results = {}
+
+        for dataset_name in dataset_names:
+            dataset = self.load_dataset(dataset_name, image_preprocess=image_preprocess, download=download)
+            
+            # For some models we just pass the PIL images, so we'll need to handle them in the collate_fn. 
+            collate_fn = _default_collate if image_preprocess is None else None
+            
+            #batch
+            joint_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=collate_fn) 
+
+            scores = model.get_retrieval_scores_batched(joint_loader)
+            result_records = dataset.evaluate_scores(scores)
+            df = pd.DataFrame(result_records)
+
+            mean_acc = df['Accuracy'].mean()
+
+            results[dataset_name] = {
+                "Model": self.model_name,
+                "Accuracy": mean_acc,
+                "Seed": seed
+            }   
     
-    model, image_preprocess = load_model(model_name, pretrained, device)
-    results = {}
-
-    for dataset_name in dataset_names:
-        dataset = load_dataset(dataset_name, image_preprocess=image_preprocess, download=download)
-        
-        # For some models we just pass the PIL images, so we'll need to handle them in the collate_fn. 
-        collate_fn = _default_collate if image_preprocess is None else None
-        
-        #batch
-        joint_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=collate_fn) 
-
-        scores = model.get_retrieval_scores_batched(joint_loader)
-        result_records = dataset.evaluate_scores(scores)
-        df = pd.DataFrame(result_records)
-
-        mean_acc = df['Accuracy'].mean()
-
-        results[dataset_name] = {
-            "Model": model_name,
-            "Accuracy": mean_acc,
-            "Seed": seed
-        }   
-   
-    return results
+        return {"ARO_accuracies": results}
--- benchmarks/main_benchmarks.py
+++ benchmarks/main_benchmarks.py
@@ -1,6 +1,8 @@
 import argparse
-from benchmarks.aro.main_evaluate_aro import evaluate_open_clip_on_aro
-from benchmarks.sugarcrepe.sugarcrepe_evaluation import evaluate_open_clip_on_sugarcrepe
+import os
+import json
+from benchmarks.aro.main_evaluate_aro import ARO_evaluation
+from benchmarks.sugarcrepe.sugarcrepe_evaluation import SugarCrepe_evaluation
 from benchmarks.winoground.winoground_evaluation import Winoground_evaluation
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -8,14 +10,28 @@ _AA = parser.add_argument
 _AA("--benchmark", help="Benchmark to run.")
 _AA("--model-name", help="OpenAI CLIP model name.")
 _AA("--pretrained", help="Pretrained checkpoint for the selected model")
+_AA("--output-dir", help="Output directory to save the results.")
 
 
 def main(_A: argparse.Namespace):
     if _A.benchmark == "aro":
-        eval_results = evaluate_open_clip_on_aro(_A.model_name, _A.pretrained)
+        benchmark_module = ARO_evaluation(_A.model_name, _A.pretrained)
+        eval_results = benchmark_module.evaluate_open_clip_on_aro()
     elif _A.benchmark == "sugarcrepe":
-        eval_results = evaluate_open_clip_on_sugarcrepe(_A.model_name, _A.pretrained)
+        benchmark_module = SugarCrepe_evaluation(_A.model_name, _A.pretrained)
+        eval_results = benchmark_module.evaluate_open_clip_on_sugarcrepe()
     elif _A.benchmark == "winoground":
-        eval_results = evaluate_open_clip_on_winoground(_A.model_name, _A.pretrained)
+        benchmark_module = Winoground_evaluation(_A.model_name, _A.pretrained)
+        eval_results = benchmark_module.evaluate_open_clip_on_winoground()
+    elif _A.benchmark == "vlchecklist":
+        pass
     else:
         raise ValueError(f"Unknown benchmark: {_A.benchmark}")
+    
+    print(eval_results)
+
+    if not os.path.exists(_A.output_DIR):
+        os.makedirs(_A.output_DIR, exist_ok=True)
+    
+    with open(os.path.join(_A.output_DIR, f"{_A.benchmark}_{_A.model_name}_{_A.pretrained}_results.json"), "w") as f:
+        json.dump(eval_results, f)
